aa375724c9d76b6991bac5a1ea1bbc8606c311ff
k <k>
2020-12-23 23:01:29 +0530

    1st commit

---
+++ appium/src/test/java/appium/Demo.java
@@ -0,0 +1,53 @@
+package appium;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+    WebDriver driver;
+
+    @BeforeMethod
+	@BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("automationName" , "UiAutomator2");
+        caps.setCapability("platformName" , "Android");
+        caps.setCapability("platformVersion" , "9.0");
+        caps.setCapability("deviceName" , "Android");
+        caps.setCapability("app" , System.getProperty("user.dir")+"/selendroid-test-app-0.17.0.apk");
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
+    }
+
+    @Test
+    public void test_Appium() throws MalformedURLException, InterruptedException {
+        String message = "Hello GitHub Actions";
+        driver.navigate().back();
+        WebElement messageTxt = driver.findElement(By.id("my_text_field"));
+        messageTxt.sendKeys(message);
+        System.out.println(messageTxt.getText());
+        AssertJUnit.assertEquals(message,messageTxt.getText());
+    }
+
+    @AfterMethod
+	@AfterClass
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
